7093a20d6e022a86af63f146c3dfaf255c9c8b51
Josh Davis <josh@joshldavis.com>
2014-01-22 12:49:43 -0600

    Add rest of basic functions, add boilerplate for f

--- week02/section2.rkt
+++ week02/section2.rkt
@@ -11,7 +11,6 @@
 ;       - Cons
 ;       - Car
 ;       - Cdr
-;       - List
 ;       - Cond
 ;       - Let
 ;   - Recursion
@@ -186,7 +185,7 @@
 
   (define (comparisons)
 
-    (eq? 'hello 'hello
+    (eq? 'hello 'hello)
     (eq? 'apple 'apple)
     (eq? 'apple "apple")
 
@@ -245,9 +244,206 @@
 
   (define (comparisons)
 
-    (eq? 'hello 'hello
+    (eq? 'hello 'hello)
     (eq? 'apple 'apple)
     (eq? 'apple "apple")
 
     )
   )
+
+;
+; Procedures
+;
+
+(define (lambdas)
+  ;
+  ; Examples
+  ;
+
+  (define (examples)
+
+    ; Identity
+    (lambda (x) x)
+
+    ; Squared
+    (lambda (x) (* x x))
+
+    ; Application
+    (lambda (f x) (f x))
+
+    )
+
+  ;
+  ; Predicates
+  ;
+
+  (define (predicates)
+
+    (procedure? 1)
+    (procedure? "hi")
+
+    (procedure? (lambda (x) x))
+
+  )
+  )
+
+;
+; Define
+;
+
+(define (def)
+
+  ; Simple numbers
+  (define x 2)
+  (define y 3)
+
+  ; Define a procedure
+  (define f1
+    (lambda (x) x))
+
+  ; This is the same as the previous define
+  (define (f2 x) x)
+
+  ; Question:
+  ;   What is this idea called?
+  ;   Hint: It has to do with sugar.
+
+  ; Question:
+  ;   Can you think of an example of this in another language that you use
+  ;   (Python, JavaScript, C)?
+  )
+
+;
+; Cons
+;
+
+(define (fcons)
+
+  ; Simple
+  (cons
+    1
+    (list 2 3 4 5))
+
+  ; Complex
+  (cons
+    1
+    (cons
+      2
+      (cons
+        3
+        (cons
+          4
+          (cons
+            5
+            (list))))))
+  )
+
+
+;
+; Car
+;
+
+(define (fcar)
+
+  ; Simple
+  (define l '(1 2 3 4))
+  (car l)
+
+  ; Little more involved
+  ; ((1 2) 3 4)
+  (define l '((1 2) 3 4))
+  (car l)
+
+  ; What happens here?
+  (define l '())
+  (car l)
+
+  )
+
+;
+; Cdr
+; Pronounced: cudder
+;
+
+(define (fcdr)
+  ; Simple
+  (define l '(1 2 3 4))
+  (cdr l)
+
+  ; Little more involved
+  ; ((1 2) 3 4)
+  (define l '((1 2) 3 4))
+  (cdr l)
+
+  ; One element?
+  ; (1)
+  (define l '(1))
+  (cdr l)
+
+  ; What happens here?
+  (define l '())
+  (cdr l)
+  )
+
+;
+; Cond
+;
+
+(define (fcond)
+  ; Simple
+  (define x 1)
+  (cond
+    ((= x 1) 1)
+    (else 2)
+    )
+
+  (define x 2)
+  (cond
+    ((= x 1) 1)
+    (else 2)
+    )
+
+  (define x 4)
+  (cond
+    ((= x 1) 1)
+    ((= x 2) 2)
+    ((= x 3) 3)
+    ((= x 4) 4)
+    (else 5))
+  )
+
+(define (flet)
+
+  ; Simple
+  (define x 3)
+  (let
+    ([x 2])
+    (displayln x))
+  (displayln x)
+
+
+  ; Multiple variables
+  (let
+    ([x 1]
+     [y 2])
+    (displayln x)
+    (displayln y))
+  )
+
+;
+; Recursion!
+;
+
+; Sum all integers from 1 to n
+(define (sum n)
+  'unimplemented)
+
+; Create integer range from p to r
+(define (range p r)
+  'unimplemented)
+
+; Basic sequential search:
+;   Return #t if q is found in l
+;   Else #f
+(define (seq-search q l)
+  'unimplemented)
