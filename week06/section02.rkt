72e34108e2076905adab8fe2c5e950bfe2f63bfd
Josh Davis <josh@joshldavis.com>
2014-02-19 17:09:00 -0600

    Add examples that we did in recitation

--- week06/section02.rkt
+++ week06/section02.rkt
@@ -5,3 +5,28 @@
 ; 4pm Recitation w/ Josh Davis
 ;
 
+; Problem 10 from homework 03
+(define-syntax-rule (for {var <- value-range} yield result)
+  (map
+    (lambda (var) result)
+    value-range
+   )
+  )
+
+
+; Problem 11 from homework 03
+(define-syntax-rule
+  (partially-applied-fun (x y) body)
+  (lambda (x y)
+    (cond
+      ((and
+         (eq? '_ x)
+         (eq? '_ y)) (lambda (x y) body))
+      ((eq? '_ x) (lambda (x) body))
+      ((eq? '_ y) (lambda (y) body))
+      (else
+        body
+        )
+      )
+    )
+  )
